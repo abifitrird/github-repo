34196b41e158691dd2f3694edfe1415a40130abb
Fitri Ratna Dewi <fratnadewi@gmail.com>
2022-03-13 18:52:40 +0700

    Use real data on app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import Owner from './components/Owner';
+import SearchField from './components/SearchField';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const getRepositories = (username) => {
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className='w-screen min-h-screen bg-pink-400 flex items-center'>
-      <div>Helloh</div>
+    <div className='w-screen min-h-screen bg-green-100'>
+      <div className='w-3/4 mx-auto py-4 space-y-3 bg-white shadow-2xl'>
+        <div className='w-full'>
+          <SearchField getData={getRepositories} />
+        </div>
+        {data.length > 0 && (
+          <div className='w-full flex flex-row'>
+            <Owner
+              avatarUrl={data[0].owner.avatar_url}
+              htmlUrl={data[0].owner.html_url}
+              username={data[0].owner.login}
+              total={data.length}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
