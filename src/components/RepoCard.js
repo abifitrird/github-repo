6d0b77ef04b95aea4c3013e452f507ee81a63ce3
Fitri Ratna Dewi <fratnadewi@gmail.com>
2022-03-13 21:08:08 +0700

    Add styling on repository detail

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -9,6 +9,14 @@ const RepoCard = ({
   updatedAt,
   topics,
 }) => {
+  const created = new Date(createdAt);
+  const updated = new Date(updatedAt);
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
   return (
     <div className='mx-2 p-4 rounded shadow-md bg-green-50'>
       <h2>
@@ -17,16 +25,27 @@ const RepoCard = ({
         </a>
       </h2>
       <hr />
-      <p>{description}</p>
-      <div className='grid grid-cols-4'>
+      <p className='description'>{description}</p>
+      <div className='grid grid-cols-5 gap-y-1'>
         <p className='col-span-1'>Language</p>
-        <p className='col-span-3'>{language}</p>
+        <p className='col-span-4'>: {language}</p>
         <p className='col-span-1'>Date Created</p>
-        <p className='col-span-3'>{createdAt}</p>
+        <p className='col-span-4'>
+          : {created.toLocaleDateString('en-UK', options)}
+        </p>
         <p className='col-span-1'>Last Update</p>
-        <p className='col-span-3'>{updatedAt}</p>
-        <p className='col-span-1'>Topics</p>
-        <p className='col-span-3'>{topics}</p>
+        <p className='col-span-4'>
+          : {updated.toLocaleDateString('en-UK', options)}
+        </p>
+        {topics.length > 0 && <p className='col-span-1'>Topics</p>}
+        {topics.length > 0 && (
+          <p className='col-span-4 flex flex-row flex-wrap space-x-2'>
+            :{' '}
+            {topics.map((item) => (
+              <i>{item}</i>
+            ))}
+          </p>
+        )}
       </div>
     </div>
   );
