3de3f0f24214953e00157af8b66cd87600e08d68
Fitri Ratna Dewi <fratnadewi@gmail.com>
2022-03-13 21:08:44 +0700

    Add trigger button click on enter

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SearchField = ({ getData }) => {
   const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const input = document.getElementById('username');
+
+    input.addEventListener('keyup', function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById('submit').click();
+      }
+    });
+  }, []);
 
   return (
     <div className='w-full px-3 font-semibold flex flex-row items-center text-green-900'>
@@ -16,6 +27,7 @@ const SearchField = ({ getData }) => {
         onChange={(element) => setUsername(element.target.value)}
       />
       <button
+        id='submit'
         className='w-48 p-3 bg-green-300 rounded-md shadow-md font-semibold hover:text-white hover:bg-green-600 focus:outline-none'
         type='type'
         onClick={() => getData(username)}
